9d911cb5071518dcb487127560ecd0b0eb638680
joshuaeramis4@gmail.com <joshuaeramis4@gmail.com>
2026-03-14 07:14:48 +0800

    some

--- code.js
+++
@@ -1,8 +0,0 @@
-<script>
-const btn = document.getElementById("menu-btn");
-const menu = document.getElementById("mobile-menu");
-
-btn.addEventListener("click", () => {
-menu.classList.toggle("hidden");
-});
-</script>
